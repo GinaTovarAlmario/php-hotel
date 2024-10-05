d8f57f119e86d3f1c605c1e4e536931211c8bd12
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-10-05 16:10:46 +0200

    fixed some errors. Completed

--- index.php
+++ index.php
@@ -39,15 +39,32 @@
         ],
 
     ];
-    if (isset($_GET["parkingYes"])){
+    $filteredHotels = $hotels;
+    // vado a filrare hotel con parcheggio
+    if (isset($_GET["parking"]) && $_GET["parking"] === "on"){
         $filteredHotels = [];
         // vado a filtrare gli hotel con parcheggio
         foreach ($hotels as $hotel) {
-            if ($hotel["parking"]) {
+            if ($hotel["parking"] === true) {
                 $filteredHotels[] = $hotel; 
             }
         }
-        $hotels = $filteredHotels;
+    } else {
+        // altrimenti fammi vedere la lista completa
+        $filteredHotels = $hotels;
+    }
+
+    // vado a gestire il valore del voto hotel
+    if(isset($_GET["vote"]) && intval( $_GET["vote"]) >= 1 && intval($_GET["vote"]) <= 5){
+        // mi serve un array per salvare la ricerca
+        $extraFilteredHotels = [];
+        foreach( $filteredHotels as $hotel){
+            if(intval($hotel["vote"]) >= intval($_GET["vote"])){
+                $extraFilteredHotels[] = $hotel;
+            }
+        }
+    } else {
+        $extraFilteredHotels = $filteredHotels;
     }
 ?>
 <!DOCTYPE html>
@@ -65,46 +82,52 @@
     <main class="container">
         <h1 class="mt-3 mb-3">List of Hotels</h1>
         <div class="row">
-            <div class="col">
-                <form action="index.php" method="GET" class="d-flex align-items-center justify-content-around" id="filterForm">
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="parkingYes" id="parkingYes" >
-                        <label class="form-check-label" for="parkingYes">
+            <div class="col-12">
+                <form action="index.php" method="GET" class="row justify-content-center" >
+                    <div class="col-3 form-check">
+                        <label class="form-check-label" for="parking">
                             Hotel with Parking
                         </label>
+                        <input class="form-check-input" type="checkbox" name="parking" id="parking" >
                     </div>
-                    <div>
-                        <label for="howManyStars">How Many Stars</label>
-                        <input type="number" max="5" min="1" id="howManyStars" name="howManyStars" placeholder="1 to 5">
+                    <div class="col-3">
+                        <label for="vote">How Many Stars</label>
+                        <input type="number" max="5" min="0" id="vote" name="vote" placeholder="1 to 5">
                     </div>
-                    <button type="submit" class="btn btn-primary">Filter List</button>
-                    <button class="btn btn-warning">Reset Search</button>
+                    <div class="col-12 text-center mt-3 mb-3">
+                        <button type="submit" class="btn btn-primary">Filter List</button>
+                        <button class="btn btn-warning">Reset Search</button>
+                    </div>
                 </form>
             </div>
         </div>
-        <table class="table mt-3 table-warning">
-            <thead>
-                <tr>
-                    <th scope="col">NAME</th>
-                    <th scope="col">DESCRIPTION</th>
-                    <th scope="col">PARKING</th>
-                    <th scope="col">VOTE</th>
-                    <th scope="col">DISTANCE TO CENTER</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach($hotels as $hotel) {?>
-                    <tr>
-                        <td><?=$hotel["name"]?></td>
-                        <td><?=$hotel["description"]?></td>
-                        <?php $answer = $hotel["parking"]? "yes" : "no"; ?>
-                        <td><?=$answer?></td>
-                        <td><?=$hotel["vote"]?></td>
-                        <td><?=$hotel["distance_to_center"]?></td>
-                    </tr>
-                <?php }?>    
-            </tbody>
-        </table>
+        <div class="row">
+            <div class="col-12">
+                <table class="table mt-3 table-warning">
+                    <thead>
+                        <tr>
+                            <th scope="col">NAME</th>
+                            <th scope="col">DESCRIPTION</th>
+                            <th scope="col">PARKING</th>
+                            <th scope="col">VOTE</th>
+                            <th scope="col">DISTANCE TO CENTER</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach($extraFilteredHotels as $hotel) {?>
+                            <tr>
+                                <td><?=$hotel["name"]?></td>
+                                <td><?=$hotel["description"]?></td>
+                                <?php $answer = $hotel["parking"]? "yes" : "no"; ?>
+                                <td><?=$answer?></td>
+                                <td><?=$hotel["vote"]?></td>
+                                <td><?=$hotel["distance_to_center"]?></td>
+                            </tr>
+                        <?php }?>    
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </main>
 </body>
 </html>
